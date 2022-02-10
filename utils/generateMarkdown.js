9012ed52af305c49b2c4de4fa83fa5b6fc33290c
Nicholas Patano <nicholaspatano@gmail.com>
2022-02-09 21:34:07 -0500

    generate markdown added

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+function renderLicenseBadge(license) {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  
+  function generateMarkdown(data) {
+    return `# ${data.title}
+    ${renderLicenseBadge(data.license)}
+    
+    ## Description
+    ${data.description}
+    
+    ## Table Content
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Collaborators](#Collaborators)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    ## Installation
+    To install necessary dependencies, run the following command:
+    ${data.installationProcess}
+    ## Usage
+    ${data.usage}
+    ## Collaborators
+    <${data.collaborators}>
+    ## Tests
+    To run tests, run the following command:
+    ${data.tests}
+    ## Questions
+    If you have any questions about the repo, contact me directly at: ${
+      data.questions
+    }
+  `;
+  }
+  
+  module.exports = generateMarkdown;
